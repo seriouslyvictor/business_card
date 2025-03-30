f1e4e6c38d1f44de8d835d7de7e45dc72d89946e
seriouslyvictor <schiavovictor@outlook.com>
2025-03-30 18:54:34 -0300

    feat: Dynamic background

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const all_links = document.querySelectorAll(".main__link")
+const allLinks = document.querySelectorAll(".main__link")
+const allBgs = ["background.webp", "Galaxy.jpg", "Planet.jpg"]
 
-all_links.forEach((link, idx) => {
+const randomBg = function (array) {
+    let cardEl = document.querySelector(".card");
+
+    if (cardEl && array.length > 0) { // Check if element exists and array is not empty
+        let rng = Math.floor(Math.random() * array.length); // Use dynamic length
+        cardEl.style.setProperty("background-image", `url("/backgrounds/${array[rng]}")`); // Fix CSS format
+    }
+};
+allLinks.forEach((link, idx) => {
     link.classList.add("slide-in-right")
     link.style.animationDelay = `${idx * 0.2}s`; // Delays each by 0.4s incrementally
 });
+
+randomBg(allBgs)
