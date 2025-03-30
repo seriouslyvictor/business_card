49ed5d40d8b88ba6ee56f246b634739c0b0e8197
seriouslyvictor <schiavovictor@outlook.com>
2025-03-30 18:33:48 -0300

    fix: Ajustado responsividade em celulares

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const all_links = document.querySelectorAll(".main__link")
 
-
-const adjustHeight = function () {
-    let vh = window.innerHeight;
-    console.log(vh)
-    let cardEl = document.querySelector(".card")
-    cardEl.setAttribute("height", vh + "px")
-
-}
-
 all_links.forEach((link, idx) => {
     link.classList.add("slide-in-right")
     link.style.animationDelay = `${idx * 0.2}s`; // Delays each by 0.4s incrementally
 });
-
-document.addEventListener("DOMContentLoaded", adjustHeight)
